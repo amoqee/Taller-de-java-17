1840f6ebcaa3a5c94001903939b1d7040880bb42
Karen <Karen@Odette>
2016-06-16 11:13:42 -0500

    Se agrego ER2

---
+++ SyFP16/src/Examen1/Circula.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examen1;
+
+/**
+ *
+ * @author Karen
+ */
+public class Circula extends Megalopolis {
+    public String getByPlaca(String digitos ){
+        
+        if(digitos = 5,6){
+            return String lunes;
+        }
+        
+        if(digitos = 7,8){
+            return String martes;
+        }
+        
+        if(digitos = 3,4){
+            return String miercoles;
+        }
+        
+        if(digitos = 1,2){
+            return String jueves;
+        }
+        
+        if(digitos = 9,0){
+            return String viernes;
+        }  
+        
+                   
+               
+        
+        
+    }
+    
+    public String getTerminacionPlaca(String num){
+        
+        if(num = lunes){
+            return String 5, 6;
+        }
+        
+        if(num = martes){
+            return String 7, 8;
+        }
+        
+        if(num = miercoles){
+            return String 3,4;
+        }
+        
+        if(num = jueves){
+            return String 1,2;
+        }
+        
+        if(num = viernes){
+            return String 9,0;
+        }
+        
+    }
+    
+    public String getColor(String dia){
+        
+        if( dia = amarillo){
+            return String lunes;
+        }
+        
+        if( dia = rosa){
+            return String martes;
+        }
+        
+        if(dia = rojo){
+            return String miercoles;
+        }
+        
+        if(dia = verde){
+            return String jueves;
+        }
+        
+        if(dia = azul){
+            return String viernes;
+        }
+    }
+    
+}
---
+++ SyFP16/src/Examen1/Circula2.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Examen1;
+
+/**
+ *
+ * @author Karen
+ */
+public interface Circula2 {
+    
+    public String getHoynocircula(){
+                      
+       
+      
+    }
+    
+    
+    public String getByPlaca(String digitos ){
+       
+ }
+  
+    
+    public String getTerminacionPlaca(String num){
+        
+{   
+    
+    public String getColor(String dia){
+        
+       
+        
+}
+        
+        
+         
+   
+      
+   
+    
+
